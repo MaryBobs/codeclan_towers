10db41455ac0edf9d9f6b8f3fb68ad32c2c7fd3c
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-08 11:03:48 +0000

    Guest has name test passing

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    private Guest guest;
+
+    @Before
+    public void before() {
+        guest = new Guest("Eugene");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Eugene", guest.getName());
+    }
 }
