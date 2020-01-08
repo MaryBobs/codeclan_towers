fcc241bcb76bb0cbd034ba1b28590d33cb2b3d23
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-08 11:30:57 +0000

    conference room setup tests passing

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String name;
+
+    public ConferenceRoom(int capacity, String name) {
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int countGuests() {
+        return guests.size();
+    }
 }
+
+
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+    private ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+        conferenceRoom = new ConferenceRoom(10, "Conference Room 1");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(10, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Conference Room 1", conferenceRoom.getName());
+    }
+
+    @Test
+    public void startsEmpty(){
+        assertEquals(0, conferenceRoom.countGuests());
+    }
+
 }
